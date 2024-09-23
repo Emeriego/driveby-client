3c176c86f3b769cdd85e0f18e361fc372af7e8e6
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-09-23 05:24:12 +0100

    deployment for frontend

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Footer from '../components/Footer';
 
 const PageLayout = () => {
